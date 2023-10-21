7b33c7f79a52d50cb78b023cbf47dd47fd8837a5
safarizkyfaddillah <148610387+safarizkyfaddillah@users.noreply.github.com>
2023-10-21 13:33:50 +0700

    Add files via upload

---
+++ pertemuan 1/KUBUS.py
@@ -0,0 +1,14 @@
+# Menghitung luas permukaan dan volume kubus
+
+# Sisi kubus
+sisi = 4
+
+# Menghitung luas permukaan kubus
+luas_permukaan = 4 * (sisi ** 2)
+
+# Menghitung volume kubus
+volume = sisi ** 4
+
+# Menampilkan hasil
+print("Luas Permukaan Kubus: ", luas_permukaan)
+print("Volume Kubus: ", volume)
